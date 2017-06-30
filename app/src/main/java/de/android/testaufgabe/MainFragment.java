1ee087776662111616c6bc4cb11fa2b40897618c
Ayrat <ayrat1@mail.ru>
2017-06-30 09:03:48 +0300

    some changes

--- app/src/main/java/de/android/testaufgabe/MainFragment.java
+++ app/src/main/java/de/android/testaufgabe/MainFragment.java
@@ -1,9 +1,16 @@
 package de.android.testaufgabe;
 
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +19,20 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainFragment extends Fragment implements View.OnClickListener{
+public class MainFragment extends Fragment implements View.OnClickListener {
     private Button button;
     private ProgressBar progressBar;
     private TextView textView;
     private ListView listView;
     private ArrayAdapter<String> adapter;
     private List<String> arrayList;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,10 +44,10 @@ public class MainFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_main, container, false);
-        button = (Button)view.findViewById(R.id.button);
-        progressBar = (ProgressBar)view.findViewById(R.id.progressBar);
-        textView = (TextView)view.findViewById(R.id.textView);
-        listView = (ListView)view.findViewById(R.id.listView);
+        button = (Button) view.findViewById(R.id.button);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        textView = (TextView) view.findViewById(R.id.textView);
+        listView = (ListView) view.findViewById(R.id.listView);
         arrayList = new ArrayList<>();
         adapter = new ArrayAdapter<>(
                 getActivity(),
@@ -45,12 +55,55 @@ public class MainFragment extends Fragment implements View.OnClickListener{
                 arrayList);
 
         button.setOnClickListener(this);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        progressBar.setVisibility(View.VISIBLE);
+//        progressBar.setVisibility(View.VISIBLE);
+        textView.setVisibility(View.VISIBLE);
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(getActivity(), "Add permissions", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.setText(location.getLatitude() + "/" + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    @Override
+    public void onPause() {
+        locationManager.removeUpdates(locationListener);
+        super.onPause();
     }
 }
